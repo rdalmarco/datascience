1885e737b6f4acae1c915de8bfdc0b4cb90e53f9
Rafael Dalmarco <111667173+rdalmarco@users.noreply.github.com>
2023-10-26 16:44:41 -0300

    Delete list-comprehension.py

--- list-comprehension.py
+++
@@ -1,8 +0,0 @@
-numeros = list(range(1, 101))
-
-print(numeros)
-
-##Para cada numero na lista de numero, confere se é divísível por 2 e 4 e retorna o número (tudo somente na linha de código abaixo)
-pares_div4 = [numero for numero in numeros if numero % 2 == 00 and numero % 4 == 0]
-
-print(pares_div4)
